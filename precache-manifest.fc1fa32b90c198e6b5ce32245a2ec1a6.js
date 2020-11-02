313c0a3ec540310161bd43f76bb31800c1d056ad
BenjaminWalkerBond <benbond96@gmail.com>
2020-11-01 20:02:22 -0600

    Updates

---
+++ precache-manifest.fc1fa32b90c198e6b5ce32245a2ec1a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27df3fbdfb8cd79aba0c76187eb78c0a",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "dcc4bcb369ee85773c6a",
+    "url": "/dice/static/css/main.35268fd4.chunk.css"
+  },
+  {
+    "revision": "12f8f360e57227cfe91a",
+    "url": "/dice/static/js/2.3a1a4e2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice/static/js/2.3a1a4e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcc4bcb369ee85773c6a",
+    "url": "/dice/static/js/main.7e7d428a.chunk.js"
+  },
+  {
+    "revision": "6f349f4a9e93576960a4",
+    "url": "/dice/static/js/runtime-main.6581301c.js"
+  }
+]);
